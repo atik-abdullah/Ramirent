4307083f96155b50de06985471e3cfddaa94aed2
atik <ios_developer_atik@yahoo.com>
2020-03-27 07:52:06 +0200

    Second Commit - Reading database connection from envs/dev.json

--- serverless/handler.js
+++ serverless/handler.js
@@ -3,88 +3,41 @@
 const aws = require('aws-sdk');
 const Jimp = require("jimp");
 
+//DB Connection
+require("./src/database/connection");
 
-module.exports.uploadImage = (event, context, callback) => {
-  var params = {
-    FunctionName: 'serverless-dev-ResizeImage',
-    InvocationType: "Event"
-  }
 
-  var sizes = [128, 256, 1024];
+module.exports.uploadImage = async (event, context, callback) => {
+  console.log("Entering successful");
 
-  const s3Objects = event['Records'].map(function (r) {
-    return r["s3"]
-  })
-
-  const lambda = new aws.Lambda({
-    region: 'us-east-1'
+  //Require models
+  const Answer = require("./src/model/Answer");
+  const Form = require("./src/model/Form");
+  //Create Relations 
+  Form.hasMany(Answer, {
+    as: "answers",
+    foreignKey: "userForeignKeyId"
+  });
+  Answer.belongsTo(Form, {
+    as: "forms",
+    foreignKey: "userForeignKeyId"
   });
 
-  for (var i = 0; i < sizes.length; i++) {
-    params['Payload'] = JSON.stringify({
-      "size": sizes[i],
-      "s3Objects": s3Objects
-    });
-
-    lambda.invoke(params, function (error, data) {
-      if (error) {
-        callback(error)
-      } else {
-        callback(null, 'success')
-      }
-    });
-  }
-
-};
-
-
-module.exports.resizeImage = (event, context, callback) => {
-
-  const size = event.size;
-  const S3 = new aws.S3();
-
-  event.s3Objects.map(function (s3Object) {
-    var bucket = s3Object.bucket.name;
-    var key = s3Object.object.key;
-    var parts = key.split('.');
-    var name = parts[0];
-    var suffix = parts[1];
-
-    function uploadToS3(err, buffer) {
-      const keyName = name + "-" + size + "." + suffix
-      var params = {
-        Body: buffer,
-        Bucket: 'destinationbucket100',
-        Key: keyName
-      }
-
-      S3.putObject(params, function (err, data) {
-        if (err) {
-          callback(err);
-        } else {
-          console.log('successfully uploaded resized image: ' + keyName)
-          callback(null, "success");
-        }
-      })
-    }
-
-    S3.getObject({
-      Bucket: bucket,
-      Key: key
-    }, function (err, data) {
-      if (err) {
-        console.log('Error reading S3 item: ' + bucket + ' ' + key);
-      } else {
-        Jimp.read(data.Body, function (err, buffer) {
-          buffer
-            .resize(size, Jimp.AUTO)
-            .getBuffer(Jimp.MIME_JPEG, uploadToS3)
-        })
-      }
-    });
-
-    callback(null, "success");
-
-  });
-
+  //Generic Error Handler 
+  const errHandler = err => {
+    //Catch and log any error.
+    console.error("Error: ", err);
+  };
+  //create returns a promise which gets resolved to the user instance 
+  //We also use await, you can use standard then callback.
+  const form = await Form.create({
+    formId: "alexdmc1",
+    title: "1234567890"
+  }).catch(errHandler); ///< Catch any errors that gets thrown
+  //You must provide the userId to get each tweet linked to a single user.
+  const answer = await Answer.create({
+    label: "This is One of the form answer",
+    userId: form.id
+  }).catch(errHandler);
+  console.log("Finishing successful");
 };
